5f84898d847f86816a3d4d14c763874e0a4187ab
Julio Rodriguez <julioguez@gmail.com>
2014-12-20 09:46:35 -0600

    Se agrego la funcionalidad al menu de ocultarse cuando se da click sobre un enlace del mismo.

--- statics/js/main.js
+++ statics/js/main.js
@@ -3,9 +3,18 @@ $(function() {
     var btnMenu = $('#Mobile-icon');
     $(btnMenu).on('click', showMenu);
 
+    // hide & show menu when click on the icon menu
     function showMenu(event) {
         event.preventDefault();
         $('#Menu').slideToggle();
+    }
+
+    // hide the menu when click on links
+    var menuLink = $('.Menu-link');
+    $(menuLink).on('click', hideMenu);
+
+    function hideMenu(event) {
+        $('#Menu').hide(500);
     }
 
 });
